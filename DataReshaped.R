29d589eb0ea05d71832e5cecb6a6540df546b726
alexspiers <advspiers@gmail.com>
2014-10-12 17:47:02 +0100

    Added code for reading the data so plot can be fully reproduced

---
+++ DataReshaped.R
@@ -0,0 +1,4 @@
+powerdata <- read.table("./exdata-data-household_power_consumption/household_power_consumption.txt",  header = TRUE, sep = ";", na.strings = "?")
+Feb07 <- subset(powerdata, Date == "1/2/2007" | Date == "2/2/2007")
+Feb07$DateAndTime <- paste(Feb07$Date, Feb07$Time)
+PowerData <- subset(Feb07, select = 3:10)
